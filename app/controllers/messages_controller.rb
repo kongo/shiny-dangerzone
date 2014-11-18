f8e41f5b323b64d43d8d91c66a0fe8645b036c21
kongo <d.parshenko@gmail.com>
2014-11-18 17:43:08 +0200

    Create new message feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,17 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+
+  def create
+    @message = Message.new message_params
+    render(:new) && return unless @message.valid?
+
+    @message.write_to_file_storage
+    flash[:success] = 'Message sent'
+    redirect_to root_path
+  end
+
+  def message_params
+    params.require(:message).permit(Message::PUBLIC_ATTRS)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,12 @@
+require 'csv'
+
 class Message < ActiveRecord::Base
+  STORAGE_FILE_NAME = Rails.root.join('tmp').join('messages.csv')
+  PUBLIC_ATTRS = ["name", "email", "subject", "contents"]
+
+  def write_to_file_storage
+    CSV.open(STORAGE_FILE_NAME, 'ab', col_sep: "\t") do |f|
+      f << self.attributes.slice(*PUBLIC_ATTRS).values
+    end
+  end
 end
