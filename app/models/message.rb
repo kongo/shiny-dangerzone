8464692b93e2d62f64ec84a223ba9d307971e9cc
kongo <d.parshenko@gmail.com>
2014-11-18 17:49:26 +0200

    Validate Message attributes presence

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ActiveRecord::Base
   STORAGE_FILE_NAME = Rails.root.join('tmp').join('messages.csv')
   PUBLIC_ATTRS = ["name", "email", "subject", "contents"]
 
+  validates :name, :email, :subject, :contents, presence: true
+
   def write_to_file_storage
     CSV.open(STORAGE_FILE_NAME, 'ab', col_sep: "\t") do |f|
       f << self.attributes.slice(*PUBLIC_ATTRS).values
