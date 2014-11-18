518f3515d8cdda20e2df3a65c16748c6f36211dd
kongo <d.parshenko@gmail.com>
2014-11-18 15:14:12 +0200

    Messages#new action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'messages#index'
 
+  resources :messages, only: [:index, :new, :create]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
